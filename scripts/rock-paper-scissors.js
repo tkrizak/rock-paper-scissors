d669d5fbaeea1cd7014de90058d9fcb7f7ebc3b4
Tomáš Križák <tomaskrizak@gmail.com>
2023-07-02 19:40:46 +0200

    added arrow function to autoplay

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -9,9 +9,11 @@ updateScoreElement();
 let isAutoPlaying = false;
 let intervalId;
 
+const autoPlay = () => {};
+
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function () {
+    intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
